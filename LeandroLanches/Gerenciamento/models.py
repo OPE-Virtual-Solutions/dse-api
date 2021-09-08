5bde60714fbe2a1c326b1fee122d90dd656c55b3
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-08 18:28:23 -0300

    modificado models para o novo esquema de sincronização com o banco

--- LeandroLanches/Gerenciamento/models.py
+++ LeandroLanches/Gerenciamento/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
@@ -15,7 +8,6 @@ class TbBairro(models.Model):
     taxa = models.FloatField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'TB_BAIRRO'
 
 
@@ -24,7 +16,6 @@ class TbCategoria(models.Model):
     ativo = models.BooleanField()
 
     class Meta:
-        managed = False
         db_table = 'TB_CATEGORIA'
 
 
@@ -34,9 +25,7 @@ class TbCliente(models.Model):
     telefone = models.CharField(max_length=11, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'TB_CLIENTE'
-        unique_together = (('id', 'id'),)
 
 
 class TbEndereco(models.Model):
@@ -47,7 +36,6 @@ class TbEndereco(models.Model):
     cliente = models.ForeignKey(TbCliente, models.DO_NOTHING, db_column='cliente')
 
     class Meta:
-        managed = False
         db_table = 'TB_ENDERECO'
 
 
@@ -56,9 +44,7 @@ class TbFuncionario(models.Model):
     cargo = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
         db_table = 'TB_FUNCIONARIO'
-        unique_together = (('id', 'id'),)
 
 
 class TbIngrediente(models.Model):
@@ -66,7 +52,6 @@ class TbIngrediente(models.Model):
     quantidade = models.IntegerField()
 
     class Meta:
-        managed = False
         db_table = 'TB_INGREDIENTE'
 
 
@@ -75,19 +60,16 @@ class TbIngredienteProduto(models.Model):
     ingrediente = models.ForeignKey(TbIngrediente, models.DO_NOTHING, db_column='ingrediente')
 
     class Meta:
-        managed = False
         db_table = 'TB_INGREDIENTE_PRODUTO'
 
 
 class TbItemPedido(models.Model):
-    id = models.IntegerField(primary_key=True)
     produto = models.ForeignKey('TbProduto', models.DO_NOTHING, db_column='produto')
     pedido = models.ForeignKey('TbPedido', models.DO_NOTHING, db_column='pedido', blank=True, null=True)
     quantidade = models.IntegerField()
     preco = models.FloatField()
 
     class Meta:
-        managed = False
         db_table = 'TB_ITEM_PEDIDO'
 
 
@@ -104,7 +86,6 @@ class TbPedido(models.Model):
     funcionario = models.ForeignKey(TbFuncionario, models.DO_NOTHING, db_column='funcionario', blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'TB_PEDIDO'
 
 
@@ -116,131 +97,14 @@ class TbProduto(models.Model):
     categoria = models.ForeignKey(TbCategoria, models.DO_NOTHING, db_column='categoria', blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'TB_PRODUTO'
 
 
 class TbUsuario(models.Model):
-    id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=50)
     email = models.CharField(max_length=255, blank=True, null=True)
     senha = models.CharField(max_length=255, blank=True, null=True)
     tipo = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
         db_table = 'TB_USUARIO'
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
